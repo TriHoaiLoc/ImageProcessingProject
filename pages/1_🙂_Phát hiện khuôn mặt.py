b27df17a23ccbadf0f82b32bb868226083354001
TriHoaiLoc <20133063@student.hcmute.edu.vn>
2023-05-21 18:51:36 +0700

    change

--- pages/1_🙂_Phát hiện khuôn mặt.py
+++ pages/1_🙂_Phát hiện khuôn mặt.py
@@ -19,19 +19,6 @@ class face_detect():
         )
 
     @staticmethod
-    def visualize_cam(input, faces, fps, thickness=2):
-        if faces[1] is not None:
-            for idx, face in enumerate(faces[1]):
-                coords = face[:-1].astype(np.int32)
-                cv.rectangle(input, (coords[0], coords[1]), (coords[0]+coords[2], coords[1]+coords[3]), (0, 255, 0), thickness)
-                cv.circle(input, (coords[4], coords[5]), 2, (255, 0, 0), thickness)
-                cv.circle(input, (coords[6], coords[7]), 2, (0, 0, 255), thickness)
-                cv.circle(input, (coords[8], coords[9]), 2, (0, 255, 0), thickness)
-                cv.circle(input, (coords[10], coords[11]), 2, (255, 0, 255), thickness)
-                cv.circle(input, (coords[12], coords[13]), 2, (0, 255, 255), thickness)
-        cv.putText(input, 'FPS: {:.2f}'.format(fps), (1, 16), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-
-    @staticmethod
     def visualize_img(input, faces, thickness=2):
         if faces[1] is not None:
             for idx, face in enumerate(faces[1]):
@@ -42,53 +29,7 @@ class face_detect():
                 cv.circle(input, (coords[8], coords[9]), 2, (0, 255, 0), thickness)
                 cv.circle(input, (coords[10], coords[11]), 2, (255, 0, 255), thickness)
                 cv.circle(input, (coords[12], coords[13]), 2, (0, 255, 255), thickness)
-   
-    def main_camera(self):
 
-        if 'stop' not in st.session_state:
-            st.session_state.stop = False
-            stop = False
-        
-        if self.press:
-            if st.session_state.stop == False:
-                st.session_state.stop = True
-                self.cap.release()
-                self.press.button("Start")
-            else:
-                st.session_state.stop = False
-                self.press.button("Stop")
-        
-        if 'frame_stop' not in st.session_state:
-            frame_stop = cv.imread('img\webcam-not-working.jpg')
-            st.session_state.frame_stop = frame_stop
-
-        if st.session_state.stop == True: self.FRAME_WINDOW.image(st.session_state.frame_stop, channels='BGR')
-        
-
-        frameWidth = int(self.cap.get(cv.CAP_PROP_FRAME_WIDTH))
-        frameHeight = int(self.cap.get(cv.CAP_PROP_FRAME_HEIGHT))
-        self.detector.setInputSize([frameWidth, frameHeight])
-        
-        st.subheader('Phát hiện khuôn mặt bằng camera')
-
-        while True:
-            hasFrame, frame = self.cap.read()
-            if not hasFrame:
-                break
-
-            frame = cv.resize(frame, (frameWidth, frameHeight))
-
-            # Inference
-            self.tm.start()
-            faces = self.detector.detect(frame) # faces is a tuple
-            self.tm.stop()
-
-            # Draw results on the input image
-            self.visualize_cam(frame, faces, self.tm.getFPS())
-
-            # Visualize results
-            self.FRAME_WINDOW.image(frame, channels='BGR')
-        cv.destroyAllWindows()
 
     def main_img(self):
         fileimg = st.file_uploader("Chọn 1 hình ảnh...", type=["jpg", "jpeg", "png","tif","bmp"])
@@ -114,12 +55,6 @@ if __name__=='__main__':
     st.header("Phát hiện khuôn mặt")
     st.markdown("<hr>", unsafe_allow_html=True)
     st.sidebar.markdown("---")
-    option = st.sidebar.radio('Phát hiện khuôn mặt: ',['Bằng hình ảnh','Bằng camera'])
 
     face = face_detect()
-
-    if option == "Bằng camera": 
-        face.main_camera()
-    else:
-        face.main_img()
-    
+    face.main_img()
